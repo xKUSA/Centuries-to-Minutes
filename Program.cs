a4a4b8f26d480fd7a3d2949e5613730bb34d1160
Erkan <zapochvamdneska@gmail.com>
2021-08-01 23:49:34 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter Centuries: ");
+                double centuries = Convert.ToDouble(Console.ReadLine());
+                double years = centuries * 100;
+                double days = years * 365;
+                double hours = days * 24;
+                double minutes = hours * 60;
+                double seconds = minutes * 60;
+                Console.WriteLine("Centuries {0}, Years {1}, Days {2}, Hours {3}, Minutes {4}, Seconds {5} ", centuries, years, days, hours, minutes, seconds);
+            }
+            catch
+            {
+                Console.WriteLine("MAKE SURE THAT YOU TYPED CORRECT NUMBER");
+            }
+        }        
+    }
+}
